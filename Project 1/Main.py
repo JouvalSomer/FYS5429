de1daa10014c64a31b0d29977bb46750aff23c85
Jouval Somer <jmsomer@uio.no>
2024-03-11 19:07:57 +0100

    Test of main

--- Project 1/Main.py
+++ Project 1/Main.py
@@ -1,5 +1,37 @@
+import torch
 import pandas as pd
-from argparse import ArgumentParser
 from Dataset import Data
 from Model import HydrologyLSTM
 from GeneratePlots import Plots
+from argparse import ArgumentParser
+
+
+
+
+if __name__ == "__main__":
+
+    data = pd.read_csv("ptq.txt", delimiter='\t')
+    epochs = 300
+    lookback = 360
+
+    train = 0.7
+    test = 0.2
+    validation = 0.1
+
+    myData = Data(data, lookback = lookback, split = (train, test, validation))
+
+    train = myData.train_DataLoader
+    test = myData.test_DataLoader
+    val = myData.validation_DataLoader
+    
+    model = HydrologyLSTM()
+
+    model.fit(train, 
+            epochs = 100, 
+            lr  = 0.001, 
+            store_data = False, 
+            PATH = None, 
+            n_print = 10)
+
+    pred_val_np = torch.cat(model.pred_validation_set, dim=0).numpy()
+    y_val_np = torch.cat(model.y_validation_set, dim=0).numpy()
