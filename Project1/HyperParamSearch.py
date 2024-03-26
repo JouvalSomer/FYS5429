f77d86be7e814fd283b714875c810f449f0191f9
Philip <phil.hoel@gmail.com>
2024-03-26 11:59:15 +0100

    Changed directories

---
+++ Project1/HyperParamSearch.py
@@ -0,0 +1,82 @@
+import ray
+from ray import tune
+from ray.tune import Stopper
+from ray.tune import CLIReporter
+from ray.tune.schedulers import ASHAScheduler
+
+import pandas as pd
+import numpy as np
+import json
+
+from Dataset import dataloader
+from Model import HydrologyLSTM
+
+
+class NaNOrInfStopper(Stopper):
+    def __call__(self, trial_id, result):
+        """Returns True if the trial should be stopped."""
+        return not np.isfinite(result["loss"])
+
+    def stop_all(self):
+        """Returns whether to stop all trials and prevent new ones from starting."""
+        return False
+
+
+def hyperparameter_search(config, abs_path, epochs, cpu_cores, split_ratios, num_samples=10, max_t=100, grace_period=10):
+
+    def train_model(config):
+        data = pd.read_csv(abs_path, delimiter='\t')
+        input_size = data.shape[1] - 2
+
+        dataloaders = dataloader(
+            data, lookback=config["lookback"], split_ratios=split_ratios, batch_size=config["batch_size"])
+        model = HydrologyLSTM(
+            input_size=input_size, hidden_size=config["hidden_size"], num_layers=config["num_layers"], drop_out=config["drop_out"])
+        model.fit(dataloaders, epochs=epochs, lr=config["lr"], ray_tune=True)
+
+    scheduler = ASHAScheduler(
+        metric='loss',
+        mode='min',
+        # Maximum number of training iterations (epochs) for any given trial
+        max_t=max_t,
+        # A trial must run for at least grace_period iterations before it can be stopped early.
+        grace_period=grace_period,
+        reduction_factor=2  # Determines how aggressively trials are stopped. The algorithm regularly halves the number of trials
+    )
+
+    reporter = CLIReporter(metric_columns=['loss', 'training_iteration'])
+
+    ray.init()
+
+    nan_or_inf_stopper = NaNOrInfStopper()
+
+    result = tune.run(
+        train_model,
+        # resources_per_trial={'cpu': cpu_cores, 'gpu': 1},
+        resources_per_trial={'cpu': cpu_cores},
+        config=config,
+        num_samples=num_samples,  # Number of random samples to be tested
+        scheduler=scheduler,
+        progress_reporter=reporter,
+        stop=nan_or_inf_stopper
+    )
+
+    best_trial = result.get_best_trial("loss", "min", "last")
+
+    if best_trial is None:
+        print("No successful trials.")
+        return None
+
+    best_trial_config = best_trial.config
+    best_trial_metrics = best_trial.last_result
+
+    print(f"Best trial config: {best_trial_config}")
+    print(f"Best trial final validation loss: {best_trial_metrics['loss']}")
+
+    best_trial_summary = {"config": best_trial_config,
+                          "metrics": best_trial_metrics}
+
+    with open("best_trial_summary.json", "w") as f:
+        json.dump(best_trial_summary, f, indent=4)
+
+    return best_trial_config
---
+++ Project1/Model.py
@@ -0,0 +1,159 @@
+import torch
+import torch.nn as nn
+from ray import train
+from ray import tune
+
+
+class LSTMBlock(nn.Module):
+
+    def __init__(self,
+                 input_size: int,
+                 hidden_size: int,
+                 num_layers: int
+                 ) -> None:
+
+        self.lstm_cell = nn.LSTMCell(
+            input_size=input_size, hidden_size=hidden_size)
+        self.cell_array = None
+        pass
+
+    def forward(self, x):
+        pass
+
+
+class HydrologyLSTM(nn.Module):
+
+    def __init__(self,
+                 input_size: int,
+                 hidden_size: int,
+                 num_layers: int = 1,
+                 drop_out: float = 0.4
+                 ):
+
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu')
+        super().__init__()
+        self.train_loss = []
+        self.validation_loss = []
+
+        self.y_hat_validation_set = []
+        self.y_validation_set = []
+
+        self.y_hat_train_set = []
+        self.y_train_set = []
+
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                            num_layers=num_layers, batch_first=True)
+        # self.l_norm = nn.LayerNorm(input_size)
+        self.dropout = nn.Dropout(drop_out)
+        self.fc = nn.Linear(hidden_size, 1)
+        self.relu = nn.ReLU()
+
+        self.initialize_weights()
+
+    def forward(self, x):
+
+        # x = self.l_norm(x)
+        x, _ = self.lstm(x)
+        x = self.dropout(x)[:, -1, :]
+        x = self.fc(x)
+        x = self.relu(x)
+
+        return x
+
+    def forward_predict(self, x):
+
+        # x = self.l_norm(x)
+        x, (h, c) = self.lstm(x)
+        x = self.dropout(x)[:, -1, :]
+        x = self.fc(x)
+        x = self.relu(x)
+
+        return x, c
+
+    def loss_function(self, y_pred, y):
+        mean_true = torch.mean(y)
+        numerator = torch.sum(torch.square(y - y_pred))
+        denominator = torch.sum(torch.square(y - mean_true))
+        nse_loss = numerator / denominator
+        return nse_loss
+
+    def initialize_weights(self):
+        for name, param in self.named_parameters():
+            if 'weight_ih' in name:  # Input-hidden weights
+                torch.nn.init.xavier_uniform_(param.data)
+            elif 'weight_hh' in name:  # Hidden-hidden weights
+                torch.nn.init.xavier_uniform_(param.data)
+            elif 'bias' in name:  # Bias
+                param.data.fill_(0)
+
+    def fit(self,
+            dataloaders,
+            epochs: int,
+            lr: float,
+            store_data=False,
+            PATH: str = None,
+            n_print: int = 10,
+            ray_tune=False
+            ) -> None:
+
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+        self.train()
+
+        for e in range(epochs):
+
+            running_loss = 0
+
+            for x_batch, y_batch in dataloaders['train']:
+
+                optimizer.zero_grad()
+                pred = self.forward(x_batch.to(self.device))
+
+                loss = self.loss_function(pred, y_batch.to(self.device))
+
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+
+            e_loss = running_loss / len(dataloaders['train'].dataset)
+            self.train_loss.append(e_loss)
+
+            if e % n_print == 0:
+                print(f"Epoch: {e} || Loss: {e_loss}")
+
+            if ray_tune:
+                val_loss, y_hat_set, y_set, cell_states = self.predict(
+                    dataloaders['validate'], ray_tune)
+                train.report({'loss': val_loss})
+
+        if store_data:
+            torch.save(self, PATH)
+
+    def predict(self, dataloader, ray_tune) -> tuple[float, list, list, list]:
+
+        y_hat_set = []
+        y_set = []
+        cell_states = []
+        total_loss = 0
+
+        self.eval()
+        with torch.no_grad():
+            for x_batch, y_batch in dataloader:
+                pred, cell = self.forward_predict(x_batch.to(self.device))
+
+                y_hat_set.append(pred)
+                y_set.append(y_batch)
+                cell_states.append(cell)
+
+                total_loss += self.loss_function(pred,
+                                                 y_batch.to(self.device)).item()
+
+        avg_loss = total_loss / len(dataloader.dataset)
+
+        if not ray_tune:
+            self.validation_loss.append(avg_loss)
+
+        print(f'Validation Loss: {avg_loss}')
+
+        return avg_loss, y_hat_set, y_set, cell_states
